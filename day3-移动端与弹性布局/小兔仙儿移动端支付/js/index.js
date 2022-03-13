314bfa3f0695f8508c60d4bb750a95445766066f
idleRain <gold.experience@foxmail.com>
2022-03-13 19:34:14 +0800

    day3

---
+++ day3-移动端与弹性布局/小兔仙儿移动端支付/js/index.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    let as = document.querySelectorAll('a')
+    console.log(as.length)
+    for (let i = 0; i < as.length; i++) {
+        as[i].href = 'javascript:void(0);';
+        as[i].addEventListener('click', () => {
+            alert('网页尚未完成，更多内容敬请期待…')
+        })
+    }
+    console.log(`
+ /＼　　 　 ∠＿/
+/　│　　 ／ 　／
+│　Z ＿,＜　／ 　　/ \`
+│　　　　　ヽ　  /　　 〉
+Y　　　　　\`　  /　 　/
+ｲ ●　､　●　⊂⊃ 〈　　/
+()　 へ　　　　|　＼〈
+>ｰ ､_　  ィ　  / ／ ／
+/ へ　　 /　ﾉ＜|  ＼＼
+ヽ_ﾉ　　(_／　 │／／
+7　　　　　　　|／
+＞―r￣￣\`ｰ―＿
+    `)
+}
