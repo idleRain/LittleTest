d4aa0bb1f526722cddb5f54db1e9d1fd3467da72
idleRain <gold.experience@foxmail.com>
2022-04-04 22:00:23 +0800

    day18

---
+++ VueDemo/单文件组件/School.vue
@@ -0,0 +1,31 @@
+<script>
+import Student from "./Student";
+export default {
+  name:'School',
+  data() {
+    return {
+      schoolName: '黑马程序员',
+      schoolDizhi: '武汉黄陂'
+    }
+  },
+  components:{
+    Student
+  }
+}
+// 统一暴露
+// export {school}
+// 默认暴露
+// export default school
+</script>
+<template>
+  <div class="demo">
+    <h2>{{ schoolName }}</h2>
+    <h2>{{ schoolDizhi }}</h2>
+    <Student></Student>
+  </div>
+</template>
+<style>
+.demo {
+  background: pink;
+}
+</style>
---
+++ VueDemo/单文件组件/Student.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>{{ studentName }}</h3>
+    <h3>{{ studentAge }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      studentName: '小丁',
+      studentAge: 18
+    }
+  }
+}
+</script>
